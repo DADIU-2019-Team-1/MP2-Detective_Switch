b1a2adb21a5e9e64cd755b06e6ed0e534a8f05f7
DADIUTeam1 <DADIUTeam1@outlook.com>
2019-09-19 15:46:58 +0200

    Testing pipeline
    And developing Blacklight

---
+++ Detective_Switch/Assets/Scripts/Blacklight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blacklight : MonoBehaviour
+{
+
+    public Transform effector;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(effector.position, effector.forward);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.collider.gameObject);
+            if (hit.collider.gameObject == gameObject)
+            {
+                rend.sharedMaterial.SetVector("_blacklightpos", hit.point);
+                Debug.Log("Hit the plane");
+            }
+        }
+        else
+        {
+            Debug.Log("Didn't hit plane");
+            rend.sharedMaterial.SetVector("_blacklightpos", new Vector3(0, 0, 0));
+        }
+    }
+
+}
