ba1ab3d8663a49105d3c51e09776b710864a2d59
lord001202 <42942404+lord001202@users.noreply.github.com>
2019-09-24 23:39:37 +0200

    FixedModular Setup

--- Detective_Switch/Assets/Scripts/WallManager.cs
+++ Detective_Switch/Assets/Scripts/WallManager.cs
@@ -8,53 +8,39 @@ public class WallManager : MonoBehaviour
 {
     // Controllers
     // Controllers for the modular wall setup
-    [Range(1, 3)]
+    [Range(1, 2)]
     public int WallType = 1;
 
     [Range(1, 3)]
-    public int WallColour = 1;
+    public int WallVariants = 1;
 
-    [Range(1, 3)]
-    public int PanelStyle = 1;
+    public Color WallColour = new Color(0.5f, 0.5f, 0.5f);
+    public Color PanelColour = new Color(0.6f, 0.6f, 0.6f);
 
-    [Range(1, 3)]
-    public int DoorWindowStyle = 1;
+
+
+    public bool TurnIn = false;
+    public bool TurnOut = false;
 
     // Mesh components
     // The three different base wall cutouts
     [HideInInspector] public Mesh WallStandard;
-    [HideInInspector] public Mesh WallWindow;
-    [HideInInspector] public Mesh WallDoor;
+    [HideInInspector] public Mesh WallStandardDoor;
+
+    [HideInInspector] public Mesh WallOuter;
+    [HideInInspector] public Mesh WallOuterPillar;
+    [HideInInspector] public Mesh WallOuterWindow;
 
     // Three different panelling styles (no panel, thick panel, small panel)
-    [HideInInspector] public Mesh PanelStyle01;
-    [HideInInspector] public Mesh PanelStyle02;
-    [HideInInspector] public Mesh PanelStyle03;
-    [HideInInspector] public Mesh PanelStyle01D;
-    [HideInInspector] public Mesh PanelStyle02D;
-    [HideInInspector] public Mesh PanelStyle03D;
-
-    // Three different windows (Follows panelling styles i guess)
-    [HideInInspector] public Mesh WindowStyle01;
-    [HideInInspector] public Mesh WindowStyle02;
-    [HideInInspector] public Mesh WindowStyle03;
-
-    // Three different windows (Follows panelling styles i guess)
-    [HideInInspector] public Mesh DoorStyle01;
-    [HideInInspector] public Mesh DoorStyle02;
-    [HideInInspector] public Mesh DoorStyle03;
+    [HideInInspector] public Mesh WallStandard90In;
+    [HideInInspector] public Mesh WallOuter90In;
+    [HideInInspector] public Mesh WallStandard90Out;
+    [HideInInspector] public Mesh WallOuter90Out;
 
 
     // List of usable mats
-    [HideInInspector] public Material Mat1;
     [HideInInspector] public Material Mat2;
     [HideInInspector] public Material Mat3;
-
-    // Actors
-    // Child actors
-    [HideInInspector] public GameObject window;
-    [HideInInspector] public GameObject decor;
-    [HideInInspector] public GameObject panels;
 
 
     // Start is called before the first frame update
@@ -68,118 +54,68 @@ public class WallManager : MonoBehaviour
         {
             if (WallType == 1)
             {
-                GetComponent<MeshFilter>().sharedMesh = WallStandard;
-                window.SetActive(false);
-                decor.SetActive(false);
+                if (WallVariants == 1)
+                {
+                    GetComponent<MeshFilter>().sharedMesh = WallStandard;
+                }
+                if (WallVariants == 2)
+                {
+                    GetComponent<MeshFilter>().sharedMesh = WallStandardDoor;
+                }
+                if (WallVariants == 3)
+                {
+                    GetComponent<MeshFilter>().sharedMesh = WallStandard;
+                }
+
             }
 
             if (WallType == 2)
             {
-                GetComponent<MeshFilter>().sharedMesh = WallWindow;
-                window.SetActive(true);
-                decor.SetActive(false);
+                if (WallVariants == 1)
+                {
+                    GetComponent<MeshFilter>().sharedMesh = WallOuter;
+                }
+                if (WallVariants == 2)
+                {
+                    GetComponent<MeshFilter>().sharedMesh = WallOuterPillar;
+                }
+                if (WallVariants == 3)
+                {
+                    GetComponent<MeshFilter>().sharedMesh = WallOuterWindow;
+                }
 
             }
 
-            if (WallType == 3)
+            if (TurnIn == true)
             {
-                GetComponent<MeshFilter>().sharedMesh = WallDoor;
-                window.SetActive(false);
-                decor.SetActive(true);
-            }
 
-            if (WallColour == 1)
+                if (WallType == 1)
+                {
+                    GetComponent<MeshFilter>().sharedMesh = WallStandard90In;
+                }
+                if (WallType == 2)
+                {
+                    GetComponent<MeshFilter>().sharedMesh = WallOuter90In;
+                }
+            }
+            if (TurnOut == true)
             {
-                GetComponent<MeshRenderer>().material = Mat1;
 
-            }
-
-            if (WallColour == 2)
-            {
-                GetComponent<MeshRenderer>().material = Mat2;
-
-
-            }
-
-            if (WallColour == 3)
-            {
-                GetComponent<MeshRenderer>().material = Mat3;
-
-            }
-
-            // Logic that sorts out panel styles
-            if (PanelStyle == 1)
-            {
-                if (WallType == 3)
+                if (WallType == 1)
                 {
-                    panels.GetComponent<MeshFilter>().sharedMesh = PanelStyle01D;
+                    GetComponent<MeshFilter>().sharedMesh = WallStandard90Out;
                 }
-                else
+                if (WallType == 2)
                 {
-                    panels.GetComponent<MeshFilter>().sharedMesh = PanelStyle01;
+                    GetComponent<MeshFilter>().sharedMesh = WallOuter90Out;
                 }
             }
 
-            if (PanelStyle == 2)
-            {
-                if (WallType == 3)
-                {
-                    panels.GetComponent<MeshFilter>().sharedMesh = PanelStyle02D;
-                }
-                else
-                {
-                    panels.GetComponent<MeshFilter>().sharedMesh = PanelStyle02;
-                }
-            }
+            var cubeRenderer = GetComponent<Renderer>();
 
-            if (PanelStyle == 3)
-            {
-                if (WallType == 3)
-                {
-                    panels.GetComponent<MeshFilter>().sharedMesh = PanelStyle03D;
-                }
-                else
-                {
-                    panels.GetComponent<MeshFilter>().sharedMesh = PanelStyle03;
-                }
-            }
+            cubeRenderer.sharedMaterial.SetColor("_BaseColor", WallColour);
+            cubeRenderer.sharedMaterials[1].SetColor("_BaseColor", PanelColour);
 
-            // Logic that sorts out window styles
-            if (DoorWindowStyle == 1)
-            {
-                if (WallType == 3)
-                {
-                    decor.GetComponent<MeshFilter>().sharedMesh = DoorStyle01;
-                }
-                else
-                {
-                    window.GetComponent<MeshFilter>().sharedMesh = WindowStyle01;
-                }
-            }
-
-            if (DoorWindowStyle == 2)
-            {
-                if (WallType == 3)
-                {
-                    decor.GetComponent<MeshFilter>().sharedMesh = DoorStyle02;
-                }
-                else
-                {
-                    window.GetComponent<MeshFilter>().sharedMesh = WindowStyle02;
-                }
-            }
-
-            if (DoorWindowStyle == 3)
-            {
-                if (WallType == 3)
-                {
-                    decor.GetComponent<MeshFilter>().sharedMesh = DoorStyle03;
-                }
-                else
-                {
-                    window.GetComponent<MeshFilter>().sharedMesh = WindowStyle03;
-                }
-            }
         }
     }
 }
