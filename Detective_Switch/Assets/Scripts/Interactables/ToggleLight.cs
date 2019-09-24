9e25258003d9208a5b5bdeafbe12801c71bb9157
SebastianAndresen <sebastian.sommersted@live.dk>
2019-09-24 11:45:29 +0200

    Interactable parent script begun

---
+++ Detective_Switch/Assets/Scripts/Interactables/ToggleLight.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLight : Interactable
+{
+    protected override void Action()
+    {
+        Debug.Log(testname);
+    }
+}
