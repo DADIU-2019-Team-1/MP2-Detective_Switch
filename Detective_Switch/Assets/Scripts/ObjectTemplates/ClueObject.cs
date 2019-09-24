534d969bf066e7d2c06ccbdb9677225f156d2178
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-09-24 09:44:30 +0200

    Scriptable objects setup

---
+++ Detective_Switch/Assets/Scripts/ObjectTemplates/ClueObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New clue", menuName = "Items/Clue")]
+public class ClueObject : ScriptableObject
+{
+
+    public string clueName;
+    public string clueText;
+    public Sprite clueImage;
+    public int clueID;
+    
+
+}
+
+[CreateAssetMenu(fileName = "New Key item", menuName = "Items/Keyitem")]
+public class KeyItem : ScriptableObject
+{
+    public string keyItemName;
+    public string keyItemText;
+    public Sprite keyItemImage;
+    public int keyItemID;
+}
+
+
