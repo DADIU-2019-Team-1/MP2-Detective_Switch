9d1bd5e6c6afc1ac7053f3cfe6ead7ad1b3356e6
JakobBaldwin <jakebaldwin00@gmail.com>
2019-09-25 13:18:50 +0200

    Tried Tiling didnt work

--- Detective_Switch/Assets/Scripts/TextureTilingController.cs
+++ Detective_Switch/Assets/Scripts/TextureTilingController.cs
@@ -2,48 +2,24 @@
 using System.Collections;
 
 [ExecuteInEditMode]
+
+// Update is called once per frame
 public class TextureTilingController : MonoBehaviour
 {
-
-    // Give us the texture so that we can scale proportianally the width according to the height variable below
-    // We will grab it from the meshRenderer
-    public Texture texture;
-    public float textureToMeshZ = 2f; // Use this to contrain texture to a certain size
-
-    Vector3 prevScale = Vector3.one;
-    float prevTextureToMeshZ = -1f;
-
-    // Use this for initialization
+    public GameObject WallStandard;
+    Renderer cubeRenderer;
     void Start()
     {
-        this.prevScale = gameObject.transform.lossyScale;
-        this.prevTextureToMeshZ = this.textureToMeshZ;
+        
 
-        this.UpdateTiling();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        // If something has changed
-        if (gameObject.transform.lossyScale != prevScale || !Mathf.Approximately(this.textureToMeshZ, prevTextureToMeshZ))
-            this.UpdateTiling();
+        Debug.Log(WallStandard.transform.localScale[0]);
+        Debug.Log(GetComponent<Renderer>().sharedMaterial.GetTextureScale("_MainTex"));
 
-        // Maintain previous state variables
-        this.prevScale = gameObject.transform.lossyScale;
-        this.prevTextureToMeshZ = this.textureToMeshZ;
-    }
+        GetComponent<Renderer>().material.SetTextureScale("_MainTex", new Vector2(WallStandard.transform.localScale[0], WallStandard.transform.localScale[1]));
 
-    [ContextMenu("UpdateTiling")]
-    void UpdateTiling()
-    {
-        // A Unity plane is 10 units x 10 units
-        float planeSizeX = 10f;
-        float planeSizeZ = 10f;
-
-        // Figure out texture-to-mesh width based on user set texture-to-mesh height
-        float textureToMeshX = ((float)this.texture.width / this.texture.height) * this.textureToMeshZ;
-
-        gameObject.GetComponent<Renderer>().material.mainTextureScale = new Vector2(planeSizeX * gameObject.transform.lossyScale.x / textureToMeshX, planeSizeZ * gameObject.transform.lossyScale.z / textureToMeshZ);
     }
 }
+
