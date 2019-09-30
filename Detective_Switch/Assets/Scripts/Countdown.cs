7e58031fa1692bfffc2155a83cb2e02c89e87041
lord001202 <42942404+lord001202@users.noreply.github.com>
2019-09-30 22:34:23 +0200

    SafeBeforeLanternFix

---
+++ Detective_Switch/Assets/Scripts/Countdown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    public GameObject Self;
+    float totalTime = 4f; //2 minutes
+
+    private void Update()
+    {
+        totalTime -= Time.deltaTime;
+        UpdateLevelTimer(totalTime);
+    }
+
+    public void UpdateLevelTimer(float totalSeconds)
+    {
+        int minutes = Mathf.FloorToInt(totalSeconds / 60f);
+        int seconds = Mathf.RoundToInt(totalSeconds % 60f);
+
+        string formatedSeconds = seconds.ToString();
+
+        if (seconds == 60)
+        {
+            seconds = 0;
+            minutes += 1;
+        }
+        Debug.Log(minutes.ToString("00") + ":" + seconds.ToString("00"));
+        if (seconds < 0)
+        {
+            Self.SetActive(false);
+        }
+
+    }
+}
