bde80f41f1805328e46c54018cf38b49350789fe
JFK-AudioGuy <54805890+JFK-AudioGuy@users.noreply.github.com>
2019-10-01 14:04:04 +0200

    bank change path

--- Detective_Switch/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Detective_Switch/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,31 +13,111 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_DESERT = 3303490701U;
+        static const AkUniqueID PLAY_AMB_BASIC_ROOM = 1643668981U;
+        static const AkUniqueID PLAY_AMB_ENVIRONMENT = 777179012U;
+        static const AkUniqueID PLAY_BLACKLIGHT_OFF = 1585582443U;
+        static const AkUniqueID PLAY_BLACKLIGHT_ON = 3660826327U;
+        static const AkUniqueID PLAY_CASE_FILE_BUTTON = 2410086214U;
+        static const AkUniqueID PLAY_CLOCK_DOOR_OPEN = 4011491198U;
+        static const AkUniqueID PLAY_DOOR_OPEN = 1660008929U;
+        static const AkUniqueID PLAY_DOOR_UNLOCK_OPEN = 2081382800U;
+        static const AkUniqueID PLAY_FIREPLACE = 1501216439U;
+        static const AkUniqueID PLAY_FOOTSTEP_SURFACE = 709824872U;
+        static const AkUniqueID PLAY_GAMEFLOW_MUSIC = 2096200520U;
+        static const AkUniqueID PLAY_GRAND_CLOCK_TICKING = 560436547U;
+        static const AkUniqueID PLAY_INVENTORY_BUTTON = 990055725U;
+        static const AkUniqueID PLAY_LARGE_CUPBOARD_OPEN = 95859917U;
+        static const AkUniqueID PLAY_MENU_BUTTON = 1109058446U;
+        static const AkUniqueID PLAY_MOVING_OBJECT_INTERACTIVE = 134964135U;
+        static const AkUniqueID PLAY_OPEN_DRAWER_BIG = 2407964245U;
+        static const AkUniqueID PLAY_OPEN_DRAWER_SMALL = 684873218U;
+        static const AkUniqueID PLAY_OPEN_J_BOX = 1559052575U;
+        static const AkUniqueID PLAY_PAGE_TURN = 786024383U;
+        static const AkUniqueID PLAY_PAINTING_FALLING = 2362235562U;
+        static const AkUniqueID PLAY_PILLOW_MOVE = 2612059735U;
+        static const AkUniqueID PLAY_PULL_CURTAINS = 304507835U;
+        static const AkUniqueID PLAY_SCRIBBLE = 806548754U;
+        static const AkUniqueID PLAY_STANDER_LAMP_OFF = 3906081382U;
+        static const AkUniqueID PLAY_STANDER_LAMP_ON = 3153651208U;
+        static const AkUniqueID PLAY_TABLE_LAMP_OFF = 278845169U;
+        static const AkUniqueID PLAY_TABLE_LAMP_ON = 1211107685U;
+        static const AkUniqueID PLAY_WRITING_DESK_OPEN = 2047012289U;
+        static const AkUniqueID STOP_OBJECT_MOVE = 3975567679U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMESTATE
+        {
+            static const AkUniqueID GROUP = 4091656514U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID PLAYING = 1852808225U;
+            } // namespace STATE
+        } // namespace GAMESTATE
+
+    } // namespace STATES
 
     namespace SWITCHES
     {
-        namespace CLUES_GROUP
+        namespace FS_MATERIAL
         {
-            static const AkUniqueID GROUP = 1333213729U;
+            static const AkUniqueID GROUP = 20433824U;
 
             namespace SWITCH
             {
+                static const AkUniqueID FS_CARPET = 3163497766U;
+                static const AkUniqueID FS_MARBLE = 889971200U;
+                static const AkUniqueID FS_WOOD = 3616930680U;
             } // namespace SWITCH
-        } // namespace CLUES_GROUP
+        } // namespace FS_MATERIAL
+
+        namespace GAMEPLAY
+        {
+            static const AkUniqueID GROUP = 89505537U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AFTER_DOLL = 672167899U;
+                static const AkUniqueID DOWNSTAIRS = 4106209011U;
+                static const AkUniqueID UPSTAIRS = 769052060U;
+            } // namespace SWITCH
+        } // namespace GAMEPLAY
+
+        namespace PROGRESSION
+        {
+            static const AkUniqueID GROUP = 1957646882U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LVL_00 = 2147068364U;
+                static const AkUniqueID LVL_01 = 2147068365U;
+                static const AkUniqueID LVL_02 = 2147068366U;
+                static const AkUniqueID LVL_03 = 2147068367U;
+                static const AkUniqueID LVL_04 = 2147068360U;
+                static const AkUniqueID LVL_05 = 2147068361U;
+            } // namespace SWITCH
+        } // namespace PROGRESSION
 
     } // namespace SWITCHES
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID DAY_NIGHT_RAIN = 3291295719U;
         static const AkUniqueID DENSITY_FA_SSGRAIN = 2715217995U;
         static const AkUniqueID IMMERSION_FA_SSGRAIN = 2481728872U;
         static const AkUniqueID INSTRUMENT_FA_SSGRAIN = 2317409760U;
+        static const AkUniqueID MUSIC_SLIDER = 1127857866U;
         static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID PLAYER_NEAR_WINDOW = 2962347862U;
+        static const AkUniqueID PROGRESSION = 1957646882U;
         static const AkUniqueID PROXIMITY_FA_SSGRAIN = 1791284502U;
+        static const AkUniqueID ROOM_SIZE = 2021037140U;
         static const AkUniqueID RPM = 796049864U;
         static const AkUniqueID RPM_FA_SSGRAIN = 1656280998U;
+        static const AkUniqueID SFX_SLIDER = 4140634290U;
         static const AkUniqueID SIMULATION_FA_SSGRAIN = 2428833394U;
         static const AkUniqueID SS_AIR_FEAR = 1351367891U;
         static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
@@ -59,9 +139,27 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMB_EMITTERS = 316118591U;
+        static const AkUniqueID AMB_ENVIRONMENT = 2803241449U;
+        static const AkUniqueID AMBIENCES = 1017660616U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
-        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID NON_WORLD = 838047381U;
+        static const AkUniqueID OBJECTS = 1695690031U;
+        static const AkUniqueID OBTAINABLES = 2726047681U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID SOUNDFX = 2810670744U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID WORLD = 2609808943U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID NON_WORLD_VERB = 4264461689U;
+        static const AkUniqueID PRESENCE_VERB = 2695769798U;
+        static const AkUniqueID WORLD_VERB = 2667095749U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
