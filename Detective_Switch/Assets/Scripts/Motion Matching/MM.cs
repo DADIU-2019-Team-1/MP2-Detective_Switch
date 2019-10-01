f1b0dcf36ef71363fb68a1c8d9b4e7c0cc497c83
Jannik15 <jneerd15@student.aau.dk>
2019-10-01 15:51:14 +0200

    mm

--- Detective_Switch/Assets/Scripts/Motion Matching/MM.cs
+++ Detective_Switch/Assets/Scripts/Motion Matching/MM.cs
@@ -13,20 +13,12 @@ public class MM : MonoBehaviour
      * and applies motion matching to the character.
      */
 
-    // TODO: First find the best matches for curve matching, then find the best pose in these curve animations
-
-    // TODO: Implement a culling of recently played animation frames.
-
-    // TODO: Take the player trajectory from player class (DONE), create a method for finding candidates (5 best?!) and comparing it to the player
-    // TODO: trajectory, then whichever candidates pass the trajectory match, will be compared with posematching, where best becomes new (based on ID?)
-
     // --- References
     private MMPreProcessing preprocess;
     private TrajectoryTest movement;
-    private Trajectory trajectory;
     private List<Trajectory> animTrajectories;
+    Trajectory[] animTrajectoriesInCharSpace;
     private Trajectory movementTrajectory;
-    private MMPose pose;
     private Animator animator;
 
     // --- Public variables
@@ -40,26 +32,27 @@ public class MM : MonoBehaviour
     [SerializeField] bool isMMRunning;
     [SerializeField] int framesToCull = 10;
     private AnimationClip[] allClips;
-    private float normalizedFrameTime;
-    private int candidateId;
+    [SerializeField] private int candidateId;
     private List<Trajectory> candidates;
-
+    private Queue<int> culledIDs;
 
     void Start()
     {
-        // Data is loaded from the MMPreProcessor
+        // --- Loading references
         preprocess = GetComponent<MMPreProcessing>();
         movement = GetComponent<TrajectoryTest>();
-
-
-        // Animator is initialized
         animator = GetComponent<Animator>();
+
+        // --- Initializing collections
         allClips = animator.runtimeAnimatorController.animationClips;
-
-        // Populate trajectory
         animTrajectories = preprocess.GetTrajectories();
+		animTrajectoriesInCharSpace = new Trajectory[animTrajectories.Count];
         movementTrajectory = new Trajectory(movement.GetMovementTrajectoryPoints());
-
+        culledIDs = new Queue<int>();
+        for (int i = 0; i < allClips[0].length * allClips[0].frameRate; i++)
+        {
+	        culledIDs.Enqueue(i);
+        }
 
         // Play the default animation and update the reference
         currentClip = allClips[0];
@@ -67,36 +60,53 @@ public class MM : MonoBehaviour
         currentAnimId = 0;
         PlayAnimationAtFrame(currentClip.name, currentFrame / currentClip.frameRate, 0);
 
-        // TODO: Make coroutines for starting mm, idle, and walking, as well as stopping mm
+		// Start the idle coroutine
         StartCoroutine(StartMM());
+    }
+
+    private void Update()
+    {
+	    if (movement.rootVel.sqrMagnitude >= 0.0001f && !isMMRunning)
+	    {
+		    Debug.Log("Starting MM Coroutine!");
+            StopAllCoroutines();
+		    StartCoroutine(StartMM());
+	    }
+	    if (movement.rootVel.sqrMagnitude < 0.0001f && isMMRunning)
+        { 
+			Debug.Log("Starting Idle Coroutine!");
+		    StopAllCoroutines();
+		    isMMRunning = false;
+		    StartCoroutine(StartIdle());
+	    }
     }
 
     List<Trajectory> TrajectoryMatching(Trajectory movement, float threshold)
     {
-	    List<Trajectory> trajectoryCandidates = new List<Trajectory>();
-		// Transform animation trajectories to character space
-		//for (int i = 0; i < animTrajectories.Count; i++)
-		//{
-	 //       TrajectoryPoint[] tempPoints = new TrajectoryPoint[preprocess.trajectoryPointsToUse];
-	 //       for (int j = 0; j < tempPoints.Length; j++)
-	 //       {
-		//        tempPoints[j] = new TrajectoryPoint(); // Initialize
-	 //       }
-  //          for (int j = 0; j < preprocess.trajectoryPointsToUse; j++)
-	 //       {
-		//		tempPoints[j].position = transform.worldToLocalMatrix.MultiplyPoint3x4(animTrajectories[i].GetTrajectoryPoints()[j].position);
-		//		tempPoints[j].forward = transform.worldToLocalMatrix.MultiplyVector(animTrajectories[i].GetTrajectoryPoints()[j].forward);
-  //          }
-  //          trajectoryCandidates.Add(new Trajectory(animTrajectories[i].GetClipName(), animTrajectories[i].GetFrame(), animTrajectories[i].GetTrajectoryId(), tempPoints));
-  //      }
-        for (int i = 0; i < trajectoryCandidates.Count; i++)
+        //Transform animation trajectories to character space
+
+        for (int i = 0; i < animTrajectories.Count; i++)
         {
-            if (trajectoryCandidates[i].GetTrajectoryId() >= 0 && trajectoryCandidates[i].GetTrajectoryId() != currentAnimId)
+            TrajectoryPoint[] tempPoints = new TrajectoryPoint[preprocess.trajectoryPointsToUse];
+            for (int j = 0; j < tempPoints.Length; j++)
+                tempPoints[j] = new TrajectoryPoint(); // Initialize
+            for (int j = 0; j < preprocess.trajectoryPointsToUse; j++)
             {
-                if (trajectoryCandidates[i].CompareTrajectoryPoints(movement) +
-                    trajectoryCandidates[i].CompareTrajectoryForwards(movement) < threshold)
+                tempPoints[j].position = transform.worldToLocalMatrix.MultiplyPoint3x4(animTrajectories[i].GetTrajectoryPoints()[j].position);
+                tempPoints[j].forward = transform.worldToLocalMatrix.MultiplyVector(animTrajectories[i].GetTrajectoryPoints()[j].forward);
+            }
+            animTrajectoriesInCharSpace[i] = new Trajectory(animTrajectories[i].GetClipName(), animTrajectories[i].GetFrame(), animTrajectories[i].GetTrajectoryId(), tempPoints);
+        }
+		List<Trajectory> trajectoryCandidates = new List<Trajectory>();
+		for (int i = 0; i < animTrajectoriesInCharSpace.Length; i++)
+        {
+            if (animTrajectoriesInCharSpace[i].GetTrajectoryId() >= 0 && animTrajectoriesInCharSpace[i].GetTrajectoryId() != currentAnimId
+                && !culledIDs.Contains(animTrajectoriesInCharSpace[i].GetTrajectoryId()))
+            {
+                if (animTrajectoriesInCharSpace[i].CompareTrajectoryPoints(movement) +
+                    animTrajectoriesInCharSpace[i].CompareTrajectoryForwards(movement) < threshold)
                 {
-                    trajectoryCandidates.Add(trajectoryCandidates[i]);
+                    trajectoryCandidates.Add(animTrajectoriesInCharSpace[i]);
                 }
             }
         }
@@ -118,7 +128,6 @@ public class MM : MonoBehaviour
                 newId = candidate.GetTrajectoryId();
             }
         }
-
         return newId;
     }
 
@@ -138,6 +147,8 @@ public class MM : MonoBehaviour
                 currentClip = allClips[i];
                 currentFrame = (int)(time * currentClip.frameRate);
                 currentAnimId = clipId;
+				if (isMMRunning)
+					UpdateQueue(currentAnimId);
                 return;
             }
         }
@@ -148,27 +159,26 @@ public class MM : MonoBehaviour
         if (Application.isPlaying)
         {
             Gizmos.color = Color.blue;
-            for (int i = 0; i < animTrajectories[currentAnimId].GetTrajectoryPoints().Length; i++)
+            for (int i = 0; i < animTrajectoriesInCharSpace[currentAnimId].GetTrajectoryPoints().Length; i++)
             {
-                Gizmos.DrawWireSphere(animTrajectories[currentAnimId].GetTrajectoryPoints()[i].position, movement.gizmoSphereSize);
-                Gizmos.DrawLine(animTrajectories[currentAnimId].GetTrajectoryPoints()[i].position, animTrajectories[currentAnimId].GetTrajectoryPoints()[i].position + animTrajectories[currentAnimId].GetTrajectoryPoints()[i].forward);
+                Gizmos.DrawWireSphere(animTrajectoriesInCharSpace[currentAnimId].GetTrajectoryPoints()[i].position, movement.gizmoSphereSize);
+                Gizmos.DrawLine(animTrajectoriesInCharSpace[currentAnimId].GetTrajectoryPoints()[i].position, animTrajectoriesInCharSpace[currentAnimId].GetTrajectoryPoints()[i].position + animTrajectoriesInCharSpace[currentAnimId].GetTrajectoryPoints()[i].forward);
             }
         }
     }
 
-    private void CandidatesToCull() 
+    private void UpdateQueue(int idToCull) 
     {
-        
+        culledIDs.Enqueue(idToCull);
+        if (culledIDs.Count >= framesToCull + allClips[0].length * allClips[0].frameRate)
+	        culledIDs.Dequeue();
     }
 
     private IEnumerator StartMM()
     {
-        // TODO: Add a banlist - discard x previous frames
         isMMRunning = true;
         while (true)
         {
-            if (movement.rootVel.Equals(Vector3.zero))
-                StartCoroutine(StartIdle());
             movementTrajectory = new Trajectory(movement.GetMovementTrajectoryPoints());
             candidates = TrajectoryMatching(movementTrajectory, comparisonThreshold);
             candidateId = PoseMatching(candidates);
@@ -176,19 +186,19 @@ public class MM : MonoBehaviour
             yield return new WaitForSeconds(queryRate);
         }
     }
-
-    private IEnumerator StopMM()
-    {
-        yield return new WaitForSeconds(queryRate);
-    }
     private IEnumerator StartIdle()
     {
-        Debug.Log("Idling");
-        PlayAnimationAtFrame(preprocess.poses[0].GetClipName(), (float) preprocess.poses[0].GetFrame() / 1 / 30, 0);
-        yield return new WaitForSeconds(1);
+	    while (true)
+	    {
+		    PlayAnimationAtFrame(preprocess.poses[0].GetClipName(), (float)preprocess.poses[0].GetFrame() / 1 / 30, 0);
+		    yield return new WaitForSeconds(currentClip.length);
+        }
     }
-    private IEnumerator StartMovement()
-    {
-        yield return new WaitForSeconds(queryRate);
-    }
+
+	// StartMovement() is not implemented in this project, but the idea is that if the current trajectory is = to the desired trajectory,
+	// we stop MotionMatching and simply play the movement movement animation corresponding to the trajectories.
+	//private IEnumerator StartMovement()
+ //   {
+ //       yield return new WaitForSeconds(queryRate);
+ //   }
 }
