1f5e588bb4e67bd908d22c3c34d013755545d9e3
JFK-AudioGuy <54805890+JFK-AudioGuy@users.noreply.github.com>
2019-10-02 11:41:17 +0200

    footsteps script and colliders on player feet

---
+++ Detective_Switch/Assets/Scripts/Player/footstepsound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footstepsound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AK.Wwise.Event footStepPlay;
+   
+
+    public Material currentMatFootStep;
+
+    public string lastMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        footStepPlay.Post(gameObject);
+    }
+}
