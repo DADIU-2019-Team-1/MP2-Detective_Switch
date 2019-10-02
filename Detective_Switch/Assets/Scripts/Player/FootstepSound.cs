37101a67354c92a6cda5a06eeb266c6a3d04c534
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-10-02 10:53:49 +0200

    Added footstep

---
+++ Detective_Switch/Assets/Scripts/Player/FootstepSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepSound : MonoBehaviour
+{
+    public AK.Wwise.Event footStepPlay;
+    public AK.Wwise.Event carpetPlay;
+    public AK.Wwise.Event marblePlay;
+
+    public AK.Wwise.Event woodPlay;
+
+    public Material currentMatFootStep;
+
+    public string lastMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carpetPlay.Post(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        footStepPlay.Post(gameObject);
+    }
+}
